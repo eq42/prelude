a5a3195942b2699ba6d8c0d94e6be2fa99889d5f
C.Li <chrislitex@gmail.com>
2013-08-07 19:47:19 +0800

    python and personal settings

--- personal/personal.el
+++ personal/personal.el
@@ -16,6 +16,11 @@
 (global-linum-mode t)
 (electric-indent-mode)
 
+(defun toggle-comment-on-line ()
+  "comment or uncomment current line"
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+
 ;;; load first level subdirs
 (let ((base prelude-personal-dir))
   (add-to-list 'load-path base)
@@ -27,21 +32,25 @@
         (add-to-list 'load-path name)))))
 
 (require 'yasnippet-settings)
-
+(require 'ac-settings)
 
 (require 'compile-misc)
 
 (require 'edit-functions)
 
 (require 'python-settings)
+(require 'ruby-settings)
 
 (require 'dired-settings)
 
 
 (global-set-key (kbd "C-x y") 'ffap)
+(global-set-key (kbd "C-k") 'smart-kill)
 
 ;; workaround for smartparen mode
 (sp-local-pair 'c-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+(sp-local-pair 'css-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+
 (defun my-create-newline-and-enter-sexp (&rest _ignored)
   "Open a new brace or bracket expression, with relevant newlines and indent. "
   (newline)
@@ -61,7 +70,10 @@
 
 (global-set-key (kbd "C-S-z") 'redo) ; 【Ctrl+Shift+z】;  Mac style
 
-(prelude-ensure-module-deps '(direx))
+(prelude-ensure-module-deps '(direx o-blog emmet-mode))
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 (require 'direx)
 (global-set-key (kbd "C-x C-j") 'direx:jump-to-directory)
 
--- personal/settings/python-settings.el
+++ personal/settings/python-settings.el
@@ -1,9 +1,25 @@
 ;;; python-settings -- python-settings
 ;;; Commentary:
 ;;; Code:
-(prelude-ensure-module-deps '(jedi))
+(prelude-ensure-module-deps '(jedi ein))
+
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
+
+(require 'ein)
+
+(setq
+ python-shell-interpreter "/home/chris/py3env/bin/ipython3"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+ "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+ "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
 (provide 'python-settings)
 
 ;;; python-settings.el ends here
