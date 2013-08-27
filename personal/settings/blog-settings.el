dea4f812a6a26a64fbadde770798902bf33fd953
C.Li <chrislitex@gmail.com>
2013-08-27 10:36:08 +0800

    term shell

--- personal/settings/blog-settings.el
+++ personal/settings/blog-settings.el
@@ -1,15 +1,16 @@
+(prelude-ensure-module-deps '(htmlize))
 (add-to-list 'load-path "/media/windows7/github/org-page")
 (require 'org-page)
 (setq op/repository-directory "~/blog")
 (setq op/site-domain "http://eq42.github.io")
 (setq op/personal-disqus-shortname "eq42")
-
-(setq op/site-main-title "(eval)")
-(setq op/site-sub-title "(life universe everything)")
+(setq user-mail-address "chris.li.tex@gmail.com")
+(setq op/site-main-title "'(life universe everything)")
+(setq op/site-sub-title "==============> Happy hacking, Suck less")
 (setq op/personal-github-link "https://github.com/eq42")
-
+(setq org-src-fontify-natively t)
 ;;; the three configurations below are optional
-;;(setq op/personal-google-analytics-id "your_google_analytics_id")
+(setq op/personal-google-analytics-id "UA-43259484-1")
 (setq op/repository-org-branch "source")
 (setq op/repository-html-branch "master")
 (op/do-publication)
--- personal/settings/edit-settings.el
+++ personal/settings/edit-settings.el
@@ -1,10 +1,10 @@
 ;;; edit-settings -- edit-settings
 ;;; Commentary:
 ;;; Code:
-(prelude-ensure-module-deps '(multiple-cursors ibus))
+(prelude-ensure-module-deps '(multiple-cursors))
 (require 'smart-operator)
 (require 'multiple-cursors)
-
+(require 'ibus)
 (add-hook 'python-mode-hook
           (lambda ()
             (smart-operator-mode-on)))
